f7d0b2d491e1580daa1324acf81e997b07c93c8d
SisyphianLiger <ryan.m.williams.12@gmail.com>
2023-05-09 18:04:02 +0200

    trying something new

--- src/camera_control.c
+++ src/camera_control.c
@@ -11,54 +11,50 @@
 
 uint8_t * buffer_ptr;
 
-int init_mmap(int fd){
-    struct v4l2_requestbuffers request = {0};
-    
-    request.count = 1;
-    request.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    request.memory = V4L2_MEMORY_MMAP;
+int init_mmap(int fd)
+{
+    struct v4l2_requestbuffers req = {0};
+    req.count = 1;
+    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    req.memory = V4L2_MEMORY_MMAP;
 
-    if ( -1 == xioctl(fd, VIDIOC_REQBUFS, &request)){
-	printf("Errno: %d/n", errno);
+    if (-1 == xioctl(fd, VIDIOC_REQBUFS, &req))
+    {
         perror("Requesting Buffer");
         return 1;
     }
-    
-    struct v4l2_buffer buffer = {0};
-    buffer.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    buffer.memory = V4L2_MEMORY_MMAP;
-    buffer.index = 0;
-    
-    
-    if ( -1 == xioctl(fd, VIDIOC_QBUF, &buffer)){
-	printf("Errno: %d/n", errno);
+
+    struct v4l2_buffer buf = {0};
+    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    buf.memory = V4L2_MEMORY_MMAP;
+    buf.index = 0;
+    if(-1 == xioctl(fd, VIDIOC_QUERYBUF, &buf))
+    {
         perror("Querying Buffer");
         return 1;
     }
-    void * buffer_start = mmap(NULL, buffer.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buffer.m.offset); 
-     
-    printf("Length: %d\nAddress: %p\n", buffer.length, buffer_ptr);
-    printf("Image Length: %d\n", buffer.bytesused);
 
-    buffer_ptr = (uint8_t *) buffer_start;
+    buffer = mmap (NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
+    printf("Length: %d\nAddress: %p\n", buf.length, buffer);
+    printf("Image Length: %d\n", buf.bytesused);
 
     return 0;
 }
 
-int capture_image(int fd){
-    struct v4l2_buffer buffer = {0};
-    buffer.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    buffer.memory = V4L2_MEMORY_MMAP;
-    buffer.index = 0;
-
-    if ( -1 == xioctl(fd, VIDIOC_QBUF, &buffer)){
-	printf("Errno: %d/n", errno);
-        perror("Querying Buffer");
+int capture_image(int fd)
+{
+    struct v4l2_buffer buf = {0};
+    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    buf.memory = V4L2_MEMORY_MMAP;
+    buf.index = 0;
+    if(-1 == xioctl(fd, VIDIOC_QBUF, &buf))
+    {
+        perror("Query Buffer");
         return 1;
     }
 
-    if ( -1 == xioctl(fd, VIDIOC_STREAMON, &buffer.type)){
-	printf("Errno: %d/n", errno);
+    if(-1 == xioctl(fd, VIDIOC_STREAMON, &buf.type))
+    {
         perror("Start Capture");
         return 1;
     }
@@ -66,17 +62,27 @@ int capture_image(int fd){
     fd_set fds;
     FD_ZERO(&fds);
     FD_SET(fd, &fds);
-
     struct timeval tv = {0};
     tv.tv_sec = 2;
-
     int r = select(fd+1, &fds, NULL, NULL, &tv);
-    if ( -1 == r) {
-	printf("Errno: %d/n", errno);
+    if(-1 == r)
+    {
         perror("Waiting for Frame");
         return 1;
     }
 
+    if(-1 == xioctl(fd, VIDIOC_DQBUF, &buf))
+    {
+        perror("Retrieving Frame");
+        return 1;
+    }
+
+    int outfd = open("out.img", O_RDWR);
+    write(outfd, buffer, buf.bytesused);
+    close(outfd);
+
+    return 0;
+}
 
     if ( -1 == xioctl(fd, VIDIOC_DQBUF, &buffer)){
 	printf("Errno: %d/n", errno);
