6086c7f0970b0a4d367254f621cc57217bdd4f47
SisyphianLiger <ryan.m.williams.12@gmail.com>
2023-05-10 11:43:54 +0200

    Example from v4l2

--- src/camera_control.c
+++ src/camera_control.c
@@ -11,97 +11,12 @@
 
 uint8_t * buffer_ptr;
 
-int init_mmap(int fd)
-{
-    struct v4l2_requestbuffers req = {0};
-    req.count = 1;
-    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    req.memory = V4L2_MEMORY_MMAP;
+int init_mmap(int fd) {
 
-    if (-1 == xioctl(fd, VIDIOC_REQBUFS, &req))
-    {
-        perror("Requesting Buffer");
-        return 1;
-    }
-
-    struct v4l2_buffer buf = {0};
-    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    buf.memory = V4L2_MEMORY_MMAP;
-    buf.index = 0;
-    if(-1 == xioctl(fd, VIDIOC_QUERYBUF, &buf))
-    {
-        perror("Querying Buffer");
-        return 1;
-    }
-
-    buffer = mmap (NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
-    printf("Length: %d\nAddress: %p\n", buf.length, buffer);
-    printf("Image Length: %d\n", buf.bytesused);
-
-    return 0;
 }
 
-int capture_image(int fd)
-{
-    struct v4l2_buffer buf = {0};
-    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    buf.memory = V4L2_MEMORY_MMAP;
-    buf.index = 0;
-    if(-1 == xioctl(fd, VIDIOC_QBUF, &buf))
-    {
-        perror("Query Buffer");
-        return 1;
-    }
+int capture_image(int fd) {
 
-    if(-1 == xioctl(fd, VIDIOC_STREAMON, &buf.type))
-    {
-        perror("Start Capture");
-        return 1;
-    }
-
-    fd_set fds;
-    FD_ZERO(&fds);
-    FD_SET(fd, &fds);
-    struct timeval tv = {0};
-    tv.tv_sec = 2;
-    int r = select(fd+1, &fds, NULL, NULL, &tv);
-    if(-1 == r)
-    {
-        perror("Waiting for Frame");
-        return 1;
-    }
-
-    if(-1 == xioctl(fd, VIDIOC_DQBUF, &buf))
-    {
-        perror("Retrieving Frame");
-        return 1;
-    }
-
-    int outfd = open("out.img", O_RDWR);
-    write(outfd, buffer, buf.bytesused);
-    close(outfd);
-
-    return 0;
-}
-
-    if ( -1 == xioctl(fd, VIDIOC_DQBUF, &buffer)){
-	printf("Errno: %d/n", errno);
-        perror("Retrieving Frame");
-        return 1;
-    }
-
-    int outfd = open("out.img", O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH); 
-    printf("%d\n", outfd);
-    if ( outfd < 0 ) {
-	    printf("Errno: %d/n", errno);
-	    perror("Opening Output File");
-	    return 1;
-    }
-    printf("Hello World\n"); 
-    write(outfd, buffer_ptr, buffer.bytesused);
-    close(outfd);
-
-    return 0;
 
 }
 
